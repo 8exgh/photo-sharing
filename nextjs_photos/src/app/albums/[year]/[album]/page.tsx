e6c43686480490acae80c829b6f51a603c03e868
8exgh <123822695+8exgh@users.noreply.github.com>
2025-08-30 13:49:30 -0600

    test github action

--- nextjs_photos/src/app/albums/[year]/[album]/page.tsx
+++ nextjs_photos/src/app/albums/[year]/[album]/page.tsx
@@ -39,8 +39,8 @@ function getYouTubeVideoId(url: string): string | null {
     /(?:youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/embed\/)([^&\n?#]+)/,
     /youtube\.com\/watch\?.*v=([^&\n?#]+)/,
   ];
-  
-  for (const pattern of patterns) {
+  // TODO: testing github action, sytnax error:
+  or (const pattern of patterns) {
     const match = url.match(pattern);
     if (match) {
       return match[1];
